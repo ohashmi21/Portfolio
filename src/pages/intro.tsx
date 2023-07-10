2c3d02366af9325b6e371689624a67bc63846570
Omair Hashmi <68452122+ohashmi21@users.noreply.github.com>
2023-07-09 23:57:26 -0400

    Update intro.tsx

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -4,7 +4,6 @@ function Intro() {
     <div>
       <div id="Intro">
         <p id="personal_statement"> A Sophomore at Wayne State University. Proficient in C++, python, javascript, HTML, and CSS, and constantly expanding technical skillset through personal projects.</p>
-        <img></img>
       </div>
     </div>
     );
